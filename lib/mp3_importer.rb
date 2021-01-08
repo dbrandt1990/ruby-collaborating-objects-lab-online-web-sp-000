1514db1ec5ba041a1434755c43c86042a667dc37
Danny Brandt <dvbrandt90@gmail.com>
2021-01-08 19:01:49 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,14 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :files
+  attr_reader :path
   
   def initialize (file_path)
     @path = file_path
+    @files = []
+  end
+  
+  def files
+    @files ||= Dir.entries(@path).select {|file| file.ends_with}
   end
   
 end
